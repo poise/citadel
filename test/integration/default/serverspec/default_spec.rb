bd274faf6c4ec301e924808d02cd0551a289dd72
Noah Kantrowitz <noah@coderanger.net>
2016-05-28 11:36:57 -0700

    New syntax for serverspec backend.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -16,8 +16,7 @@
 #
 
 require 'serverspec'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
+set :backend, :exec
 
 describe file('/a') do
   its(:content) { should == 'citadel' }
